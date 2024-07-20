c4d6b6d7d4238d8b8bfab5d8b0a1618c4d38dfe2
Yuri Gomes <yuurig@gmail.com>
2024-07-20 10:27:38 -0300

    feat: init project :fire:

---
+++ src/main/java/com/github/met4tron/pc_parts/PcPartsApplication.java
@@ -0,0 +1,13 @@
+package com.github.met4tron.pc_parts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcPartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcPartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/met4tron/pc_parts/PcPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.met4tron.pc_parts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcPartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
